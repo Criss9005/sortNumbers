8aaab1c28a83b502b7ab2feb4f0ba51ef36f71cb
Criss9005 <cdjpn5@gmail.com>
2024-04-02 15:11:03 -0500

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,65 @@
-import Profile from "./profile/Profile";
-import Statistics from "./statistics/Statistics";
-import FriendList from "./friendlist/FriendList";
-import TransactionHistory from "./transactionhistory/TransactionHistory";
+import { useState } from "react"
+
+function App() {
+  
+  const numbers = [4,3,5,8,4,8,16,78,12,100,99,78,45,68,42,5,1]
+  const [order, setOrder] = useState('')
+
+  
+  function Ascending(a, b) {
+      return a - b;
+  }
+
+  function Descending(a, b) {
+      return b - a;
+  }
+
+  function evenAndOdd( selector ) { 
+    const evenNumbers = []
+    const oddNumbers = []
+    numbers.forEach(number => { 
+      if (number % 2 === 0) {
+        evenNumbers.push(number)
+       }
+      else { 
+        oddNumbers.push(number)
+      }
+    })
+    if (selector === 'even') {
+      return evenNumbers
+    }
+    else { 
+      return oddNumbers
+    }
+  }
 
 
-import user from "./user.json"
-import data from "./data.json"
-import friends from "./friends.json"
-import transactions from "./transactions.json"
 
-
-export const App = () => {
   return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        backgroundColor: '#e7ecf2',
-        flexDirection: 'column'
-      }}
-    > 
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} />
+
+
+
+    <div>
+      <h1>Sort Numbers</h1>
+      <p>Numbers = [4, 3, 5, 8, 4, 8, 16, 78, 12, 100, 99, 78, 45, 68, 42, 5, 1]</p>
+      <input list="list" placeholder="please select" onChange={e => setOrder(e.target.value)}/>
       
-      <Statistics stats={data} />
+        <datalist id="list">
+        <option>Ascending</option>
+        <option>Descending</option> 
+        <option>Even</option> 
+        <option>Odd</option> 
+
+        
+         
+      </datalist>
       
-      <FriendList friends={friends} />
-      
-      <TransactionHistory items={transactions} />
+      {(order === 'Ascending') && <p>{numbers.sort(Ascending).join(', ')}</p>}
+      {(order === 'Descending') && <p>{numbers.sort(Descending).join(', ')}</p>}
+      {(order === 'Even') && <p>{evenAndOdd('even').join(', ')}</p>}
+      {(order === 'Odd') && <p>{evenAndOdd('odd').join(', ')}</p>}
     </div>
-  );
-};
+  )
+}
+
+export default App
